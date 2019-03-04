9fbbc75d41bf52f66f1f1ef421055a8649ce19ae
smith <cmdsmith3@gmail.com>
2019-03-04 12:31:45 +0800

    增加注释

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
@@ -3,21 +3,23 @@ package com.thoughtmechanix.licenses.controllers;
 import com.thoughtmechanix.licenses.model.License;
 import com.thoughtmechanix.licenses.services.LicenseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.time.LocalDateTime;
+
+// @RestController 告诉Spring Boot是一个基于REST的服务，它将自动序列化/反序列化服务请求/响应到JSON
 @RestController
+// 使用 v1/organizations/{organizationId}/licenses 作为前缀公开所有的HTTP端点
 @RequestMapping(value="v1/organizations/{organizationId}/licenses")
 public class LicenseServiceController {
     @Autowired
     private LicenseService licenseService;
 
+    // 使用值创建一个GET端点 v1/organizations/{organizationId}/licenses/{licenseId}
     @RequestMapping(value="/{licenseId}",method = RequestMethod.GET)
-    public License getLicenses( @PathVariable("organizationId") String organizationId,
+    public License getLicenses( // 从 URL映射两个参数（organizationId和licenseId）到方法参数
+                                @PathVariable("organizationId") String organizationId,
                                 @PathVariable("licenseId") String licenseId) {
 
         //return licenseService.getLicense(licenseId);
